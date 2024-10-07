444d3e0ceea9168b5a53a42c85a1e2d165ababec
alebernini <alebernini@gmail.com>
2024-10-07 17:56:57 +0100

    desenvolvendo app.py

--- src/app.py
+++ src/app.py
@@ -1,15 +1,18 @@
 import pandas as pd
+
 from pathlib import Path
 import streamlit as st
+import plotly_express as px
+from src import funcoes
 
-df = pd.read_csv(Path("../data/turnover.csv"))
+df = pd.read_csv(Path("data\turnover.csv"))
+
+df = funcoes.dropar_na(df, 'Idade', 0.05)
+df = funcoes.limpar_outlier(df, 'RendaMensal')
+df = funcoes.limpar_outlier(df, 'Idade')
 
 st.title("Análise de Turnover")
 
-fig = px.histogram(df, x = 'RendaMensal', color = 'Turnover')
+fig = px.histogram(df, x = "RendaMensal", color = "Turnover")
 st.plotpy_chart(fig)
-
-
-
-
 
